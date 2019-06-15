8a6a1f8b62d1e592500d513944c4073fb1b12a23
AlfredoJHN <41807769+AlfredoJHN@users.noreply.github.com>
2019-06-14 23:32:57 -0600

    Test

--- ProyectoATECA/Controllers/UsuariosFuncionariosController.cs
+++ ProyectoATECA/Controllers/UsuariosFuncionariosController.cs
@@ -7,7 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using ProyectoATECA.Models;
-
+//aaa
 namespace ProyectoATECA.Controllers
 {
     public class UsuariosFuncionariosController : Controller
